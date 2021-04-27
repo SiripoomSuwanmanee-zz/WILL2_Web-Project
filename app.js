b37dc68bf9a68d0fbc8fe64155a32873c9585ad0
Sorrawit <sorrawittop1@gmail.com>
2021-04-28 00:02:24 +0700

    ggl api query db wel res as err page(on fix)

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const { OAuth2Client } = require('google-auth-library');
-const con = require('./config/db');
-const session = require('express-session');
-const { MemoryStore } = require('express-session');
-const Memorystore = require('memorystore')(session);
-
+const { OAuth2Client } = require("google-auth-library");
+const con = require("./config/db");
+const session = require("express-session");
+const { MemoryStore } = require("express-session");
+const Memorystore = require("memorystore")(session);
+const checkUser = require('./checkUser');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -16,17 +16,27 @@ app.set("view engine", "ejs");
 
 //==== Session management ====
 app.use(session({
-    cookie:{ maxAge: 24*60*1000, httpOnly: true},
-    store: new MemoryStore({
-        checkPeriod: 24*60*60*1000
-    }),
-    secret: process.env.SESSION_SECRET,
-    resve: true,
-    saveUninitialized: false
+  cookie: { maxAge: 24*60*60*1000, httpOnly: true },
+  store: new MemoryStore({
+      checkPeriod: 24*60*60*1000
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: false
 }));
+
 //==== Page Route ====
-app.get("/", function (req, res) {
-  res.render("login");
+app.get("/", (req, res) => {
+  if(req.session.user) {
+    res.redirect('/welcome');
+}
+else {
+    res.render('login');
+}
+});
+
+app.get('/welcome', checkUser, (req, res) => {
+  res.render('welcome', {user: req.session.user});
 });
 
 //==== Service ====
@@ -34,7 +44,8 @@ app.get("/", function (req, res) {
 //Google login service
 const client = new OAuth2Client(process.env.CLIENT_ID);
 app.post("/verifyUser", (req, res) => {
-  const token = req.body.tokenl;
+  const token = req.body.token;
+  console.log(token);
   if (token) {
     client
       .verifyidToken({
@@ -44,24 +55,33 @@ app.post("/verifyUser", (req, res) => {
       .then((ticket) => {
         const payload = ticket.getPayload();
         const email = payload.email;
-        //verify user with DB
-        const sql = 'SELECT userID, role FROM user WHERE email=?';
+        //==================== verify user with DB ====================
+        const sql =
+          "SELECT User_ID, User_Email, User_Role, User_status FROM user WHERE User_Email=?";
         //DB query
-        con.query(sql,[email], (err, result) => {
-            if(err){
-                console.log(err);
-                return res.status(500).send('Database error');
-            }
-            //Check whether the user is our member
-            if(result.length != 1){
-                return res.status(500).send('Not a member');
-            }
-            //Check whether the user is active
-            if(result[0].role == 0){
-                return res.status(500).send('Inactive member');
-            }
-            req.session.user = {'username': payload.name, 'userID': result[0].userID, 'role': result[0].role}; //save user data to session
+        con.query(sql, [email], (err, result) => {
+          if (err) {
+            console.log(err);
+            return res.status(500).send("Database error");
+          }
+          //Check whether the user is our member
+          if (result.length != 1) {
+            return res.status(500).send("Not a member");
+          }
+          //Check whether the user is active
+          if (result[0].status == 0) {
+            return res.status(500).send("Inactive member");
+          }
+          //check user role
+          if (result[0].role == 0) {
+            req.session.user = {
+              username: payload.name,
+              userID: result[0].userID,
+              role: result[0].role,
+              status: result[0].status,
+            }; //save user data to session
             res.send("/welcome");
+          }
         });
       })
       .catch((err) => {
@@ -75,16 +95,15 @@ app.post("/verifyUser", (req, res) => {
 });
 
 //logout
-app.get('/logout', (req, res) => {
-    // destroy all sessions
-    req.session.destroy((err) => {
-        if(err) {
-            console.log(err);
-        }
-        res.redirect('/');
-    });
+app.get("/logout", (req, res) => {
+  // destroy all sessions
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
 });
-
 
 const port = process.env.PORT;
 app.listen(port, function () {
--- public/scripts/local/login.js
+++ public/scripts/local/login.js
@@ -1,5 +1,5 @@
-function init(){
-    gapi.load('auth2', function(){
+function init() {
+    gapi.load('auth2', () => {
         gapi.auth2.init({
             client_id: '918161823107-paedatbpv5t1tq8geqnfs02fgtipg2vd.apps.googleusercontent.com'
         });
@@ -9,13 +9,28 @@ function init(){
 function signIn(){
     // alert('Test')
     let auth2 = gapi.auth2.getAuthInstance();
-    auth2.signIn({ scope: 'profile email', prompt: 'select_account'}).then(function(googleUser){
-        var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
+    auth2.signIn({ scope: 'profile email', prompt: 'select_account' }).then((googleUser) => {
+        // var profile = googleUser.getBasicProfile();
+        // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        // console.log('Name: ' + profile.getName());
+        // console.log('Image URL: ' + profile.getImageUrl());
+        // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        const id_token = googleUser.getAuthResponse().id_token;
+         
+        $.ajax({
+            type: "POST",
+            url: "/verifyUser",
+            
+            data: { token: id_token },
+            success: (response) => {
+                // alert(response);
+                window.location.replace(response);
+            },
+            error: (xhr) => {
+                alert(xhr.responseText);
+            }
+        });
+    }).catch((err) => {
+        console.log(err);
     });
 }
